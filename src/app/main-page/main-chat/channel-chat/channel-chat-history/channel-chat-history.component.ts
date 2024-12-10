c831299287c156613520344c2ed3b9d67bf5d7b4
mariuskas1 <marius.kasparek@gmail.com>
2024-12-10 10:11:36 +0100

    added seperator component

--- src/app/main-page/main-chat/channel-chat/channel-chat-history/channel-chat-history.component.ts
+++ src/app/main-page/main-chat/channel-chat/channel-chat-history/channel-chat-history.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MessageComponent } from "../../../../shared-components/message/message.component";
+import { SeperatorComponent } from '../../../../shared-components/seperator/seperator.component';
 
 @Component({
   selector: 'app-channel-chat-history',
   standalone: true,
-  imports: [MessageComponent],
+  imports: [MessageComponent, SeperatorComponent],
   templateUrl: './channel-chat-history.component.html',
   styleUrl: './channel-chat-history.component.scss'
 })
