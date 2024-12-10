aa70c8ed91d7696f72db20380009cda88f802ab6
mariuskas1 <marius.kasparek@gmail.com>
2024-12-10 09:28:00 +0100

    added message component

--- src/app/main-page/main-chat/channel-chat/channel-chat-history/channel-chat-history.component.ts
+++ src/app/main-page/main-chat/channel-chat/channel-chat-history/channel-chat-history.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MessageComponent } from "../../../../shared-components/message/message.component";
 
 @Component({
   selector: 'app-channel-chat-history',
   standalone: true,
-  imports: [],
+  imports: [MessageComponent],
   templateUrl: './channel-chat-history.component.html',
   styleUrl: './channel-chat-history.component.scss'
 })
