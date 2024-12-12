b99633054f9796a861d570ea01246be23b2375cd
mariuskas1 <marius.kasparek@gmail.com>
2024-12-12 10:17:41 +0100

    improved chat history

--- src/app/shared-components/seperator/seperator.component.ts
+++ src/app/shared-components/seperator/seperator.component.ts
@@ -8,6 +8,43 @@ import { Component, Input } from '@angular/core';
   styleUrl: './seperator.component.scss'
 })
 export class SeperatorComponent {
-  @Input() date: any;
+  @Input() date: string = '';
+  formattedDate: string = '';
 
+
+
+  ngOnInit(){
+    this.setDateInput()
+
+  }
+
+
+  setDateInput(){
+    const currentDate = new Date();
+    const yesterdayDate = new Date();
+    yesterdayDate.setDate(currentDate.getDate() - 1);
+
+    const currentDateString = this.formatDate(currentDate); 
+    const yesterdayDateString = this.formatDate(yesterdayDate); 
+
+    if (this.date === currentDateString) {
+      this.formattedDate = 'Heute'; 
+    } else if (this.date === yesterdayDateString) {
+      this.formattedDate = 'Gestern'; 
+    } else {
+      this.formattedDate = this.date; 
+    }
+
+  }
+
+
+  formatDate(date: Date){
+    const formatter = new Intl.DateTimeFormat('de-DE', {
+      weekday: 'long',  
+      day: '2-digit',  
+      month: 'long',  
+    });
+
+    return formatter.format(date);
+  }
 }
