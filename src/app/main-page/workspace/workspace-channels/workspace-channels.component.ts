79e087746c68690809865af758fa8524d73f0935
Marius Kasparek <marius.kasparek@gmail.com>
2025-01-27 12:57:53 +0100

    added welcome-channel

--- src/app/main-page/workspace/workspace-channels/workspace-channels.component.ts
+++ src/app/main-page/workspace/workspace-channels/workspace-channels.component.ts
@@ -2,12 +2,13 @@ import { Component, Output, EventEmitter, OnInit, inject, Input, SimpleChanges }
 import { trigger, style, animate, transition } from '@angular/animations';
 import { Observable } from 'rxjs';
 import { Channel } from './../../../models/channel.class';
-import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Firestore, addDoc, collection, collectionData, doc, updateDoc } from '@angular/fire/firestore';
 import { ChannelService } from '../../../services/channel.service';
 import { User } from '../../../models/user.class';
 import { ThreadService } from '../../../services/thread.service';
 import { ChatService } from '../../../services/dm-chat.service';
 import { WorkspaceService } from '../../../services/workspace.service';
+import { Message } from '../../../models/message.class';
 
 
 @Component({
@@ -46,13 +47,59 @@ export class WorkspaceChannelsComponent {
   allUserChannels: Channel[] = [];
   activeChannel?: Channel | null;
   firestore: Firestore = inject(Firestore);
-  
 
+  welcomeMessage = new Message();
+  welcomeChannel?: Channel | null;
+  
 
   ngOnInit(){
     this.subscribeToChannelService();
     this.getAllChannels();
+    setTimeout(()=>{
+      this.checkIfUserHasChannels();
+    }, 200)
+  
   }
+
+
+  async checkIfUserHasChannels(){
+    if(this.allUserChannels.length === 0){
+      await this.addUserToWelcomeChannel();
+      await this.addWelcomeMessageToWelcomeChannel();
+      const welcomeChannel = this.getWelcomeChannel();
+      
+      if(welcomeChannel){
+        this.activateChannel(welcomeChannel)
+      }
+    }
+  }
+
+  getWelcomeChannel(){
+    return this.allUserChannels.find(channel => channel.name === 'Welcome-Channel') || null;
+  }
+
+  async addUserToWelcomeChannel(){
+    const channelDocRef = doc(this.firestore, 'channels/11F7a9TqLf6BxuhTVD2H');
+    try {
+      await updateDoc(channelDocRef, {
+        userIds: this.currentUser?.id
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async addWelcomeMessageToWelcomeChannel(){
+    this.initializeNewWelcomeMessage();
+    const welcomeMsg = doc(this.firestore, 'channels/11F7a9TqLf6BxuhTVD2H/messages/hGa7XVRj51jDqXhq7gVp');
+    
+    try {
+      await updateDoc(welcomeMsg, { ...this.welcomeMessage });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
   
 
   subscribeToChannelService(){
@@ -85,6 +132,7 @@ export class WorkspaceChannelsComponent {
     const userChannelsCollection = collection(this.firestore, 'channels' );
     this.channels$ = collectionData(userChannelsCollection, { idField: 'id'}) as Observable<Channel[]>;
     
+  
     this.channels$.subscribe((changes) => {
       this.allChannels = Array.from(new Map(changes.map(channel => [channel.id, channel])).values());
       this.getAllChannelsForCurrentUser();
@@ -106,4 +154,14 @@ export class WorkspaceChannelsComponent {
     this.activeChannel = channel;
     this.channelService.setActiveChannel(channel);
   }
+
+
+  initializeNewWelcomeMessage(){
+    this.welcomeMessage.timeStamp = new Date().toISOString();
+    this.welcomeMessage.messageText = "Herzlich Willkommen auf DABubble! Hier kannst du dich in Echtzeit mit deinen Teammitgliedern austauschen, entweder per Direktnachricht oder über einen Channel, den du ganz einfach im Workspace-Menü auf der linken Seite erstellen kannst. :-)"
+    this.welcomeMessage.senderName = "Welcome-Bot";
+    this.welcomeMessage.senderId = "2BB0uxWkRwSjhhUSexrcOU9"
+    this.welcomeMessage.senderAvatar = "assets/img/bot1.png"
+  }
+
 }
