59bcbd7c2b560a8e5cfde539f34cbddcc6ce5a4b
DennisSchwarz1995 <schwarzdennis1995@gmail.com>
2025-01-28 13:06:09 +0100

    Added Footer Update on Route for Auth-Layout

--- src/app/shared-components/footer/footer.component.ts
+++ src/app/shared-components/footer/footer.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
-
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [RouterLink],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  showRegistrationLink: boolean = true;
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.updateFooterOnRoute(this.router.url);
+        this.router.events.subscribe((event) => {
+          if (event instanceof NavigationEnd) {
+            this.updateFooterOnRoute(event.urlAfterRedirects);
+          }
+        });
+  }
+
+  updateFooterOnRoute(url: string) {
+    if (url.includes('login')) {
+      this.showRegistrationLink = true;
+    }
+
+    if (url.includes('registration')) {
+      this.showRegistrationLink = false;
+    }
+  }
 }
